7b37c3438b11e5f4125b04e129cf1f6c78cdea41
SheezChill <rageendergaming@gmail.com>
2023-09-13 19:40:04 +0700

    Change fonts fetching

--- src/pages/fonts/[name].ts
+++ src/pages/fonts/[name].ts
@@ -7,9 +7,9 @@ export const GET: APIRoute = async ({ params, request }) => {
   if (!fontName) return new Response('no font name')
 
   const res = await fetch(`${url.origin}/font/${fontName}`)
-  const stream = res.body
+  const fontFile = await res.arrayBuffer()
 
-  return new Response(stream, {
+  return new Response(fontFile, {
     headers: {
       'Content-Type': 'font/woff2',
       'Cache-Control': 'public, max-age=315360000'
